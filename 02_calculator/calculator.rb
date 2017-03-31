4410f0d9c7e416a52f039db76fc052f89740d575
JoseMPena <josmpena91@gmail.com>
2017-03-31 20:13:27 +0200

    Complete 02_calculator

--- 02_calculator/calculator.rb
+++ 02_calculator/calculator.rb
@@ -1,1 +1,25 @@
 #write your code here
+
+def add(first, second)
+  first + second
+end
+
+def subtract(first, second)
+  first - second
+end
+
+def sum(array)
+  array.reduce(0, :+)
+end
+
+def multiply(array)
+  array.reduce(:*)
+end
+
+def power(base, expo)
+  base**expo
+end
+
+def factorial(num)
+  (1..num).reduce(1, :*)
+end
