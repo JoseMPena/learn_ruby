c6ae4b5d4d274ad7e04cbe72fe92acb057ec3791
JoseMPena <josmpena91@gmail.com>
2017-04-05 20:48:45 +0200

    Complete simon says

--- 03_simon_says/simon_says.rb
+++ 03_simon_says/simon_says.rb
@@ -1,1 +1,30 @@
-#write your code here
+def echo(text)
+  text
+end
+
+def shout(text)
+  text.upcase
+end
+
+def repeat(text, repeat=2)
+  repeated = ''
+  repeat.times do
+    repeated += text
+    repeated += ' ' if repeated.count('hello') < text.size * repeat
+  end
+  repeated
+end
+
+def start_of_word(word, position)
+  word[0..position - 1]
+end
+
+def first_word(sentence)
+  sentence.split(' ').first
+end
+
+def titleize(sentence)
+  sentence.split(' ').map.with_index { |word, i|
+    %w(and over the).include?(word) && !i.equal?(0) ? word : word.capitalize
+  }.join(' ')
+end
