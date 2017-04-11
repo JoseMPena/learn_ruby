2bd35fe59562262945d205add7b4197e17194a31
JoseMPena <josmpena91@gmail.com>
2017-04-11 22:29:20 +0200

    Complete pig_latin

--- 04_pig_latin/pig_latin.rb
+++ 04_pig_latin/pig_latin.rb
@@ -1,4 +1,12 @@
-def translate(string)
-  string.reverse! unless %w(a e i o u).include? string[0]
-  string + 'ay'
+def translate(sentence)
+  vowels = %w(a e i o u)
+  words = sentence.split(' ')
+  words.each do |word|
+    word.each_char do |char|
+      break if vowels.include?(char) && word[word.size - 1] != 'q'
+      word << word.slice!(0)
+    end
+    word << 'ay'
+  end
+  words.join(' ')
 end
